df8885a6d8bf716f80e814f316b8bd5d880c08bc
dgeison <dgeison.peixoto@gmail.com>
2023-06-09 14:15:09 -0300

    upload file in AWS S3

---
+++ connect_list.py
@@ -0,0 +1,14 @@
+import boto3
+from constants import *
+
+client = boto3.client('s3', 
+                      aws_access_key_id= AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+                      )
+
+response = client.list_buckets()
+
+for bucket in response['Buckets']:
+    print(bucket['Name'])
+
+#print(response)
---
+++ s3_upload.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+from constants import *
+
+client = boto3.client('s3',
+                      aws_access_key_id=AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY,)
+
+root_path = os.getcwd()
+print(f"Root path: {root_path}")
+file_name = 'arquivo.txt'
+
+file_name_path = os.path.join(root_path, 'data', file_name)
+print(f"File name path: {file_name_path}")
+
+AWS_BUCKET_NAME = 'meubucketcielo'
+client.upload_file(file_name_path, AWS_BUCKET_NAME, file_name)
