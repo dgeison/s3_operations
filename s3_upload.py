3d2faf2bca9924ee600505bbc7c635ae2bc8d201
dgeison <dgeison.peixoto@gmail.com>
2023-06-09 16:16:03 -0300

    insert single file

--- s3_upload.py
+++ s3_upload.py
@@ -8,17 +8,10 @@ client = boto3.client('s3',
 
 root_path = os.getcwd()
 print(f"Root path: {root_path}")
+file_name = 'arquivo.csv'
 
-folder_path = os.path.join(root_path, 'data')
-print(f"Folder path: {folder_path}")
-
+file_name_path = os.path.join(root_path, 'data', file_name)
+print(f"File name path: {file_name_path}")
 
 AWS_BUCKET_NAME = 'meubucketcielo'
-
-for file_name in os.listdir(folder_path):
-    file_name_path = os.path.join(folder_path, file_name)
-    print(f"File name path: {file_name_path}")
-    print(f"File name: {file_name}")
-    if os.path.isfile(file_name_path):
-        print(f"Uploading {file_name} to AWS S3 bucket {AWS_BUCKET_NAME}")
-        client.upload_file(file_name_path, AWS_BUCKET_NAME, file_name)
+client.upload_file(file_name_path, AWS_BUCKET_NAME, file_name)
